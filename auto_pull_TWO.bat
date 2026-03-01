826d0906c51f5558022429ecff55485518ffd148
Thiago Luz <thiago@example.com>
2026-03-01 17:54:42 -0300

    Make auto pull silent and regenerate zip

--- auto_pull_TWO.bat
+++ auto_pull_TWO.bat
@@ -1,15 +1,4 @@
 @echo off
-echo ==============================================
-echo  Buscando atualizacoes do projeto TWO no GitHub...
-echo ==============================================
-echo.
-
 cd /d "%~dp0"
 git pull origin main
-
-echo.
-echo ==============================================
-echo  Atualizacao concluida!
-echo  Seus arquivos locais agora estao iguais aos do GitHub.
-echo ==============================================
-pause
+powershell -Command "Compress-Archive -Path 'src-extensao\*' -DestinationPath 'assets\extensao.zip' -Force"
